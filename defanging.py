af4d62908da52d04644ff32e716fd6658db10ae0
Farheena12 <70895688+Farheena12@users.noreply.github.com>
2024-08-06 20:48:45 +0530

    Update defanging.py

--- defanging.py
+++ defanging.py
@@ -1,12 +1,20 @@
 #https://leetcode.com/problems/defanging-an-ip-address/description/
 
+#Method 1: Using if and for loop
+# class Solution:
+#     def defangIPaddr(self, address: str) -> str:
+#         res = ""
+#         for i in address:
+#             if i ==".":
+#                 res = res + "[.]"
+#             else:
+#                 res = res + i
+#         return res  
+
+Method 2: Using replace method.
 class Solution:
     def defangIPaddr(self, address: str) -> str:
-        res = ""
-        for i in address:
-            if i ==".":
-                res = res + "[.]"
-            else:
-                res = res + i
-        return res  
+        address = address.replace('.', "[.]")
+        return address  
+        
         
